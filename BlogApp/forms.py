baaf36cec5da44a5c1342323936b76b61f42e494
NicacioSilva <nas@cesar.school>
2020-05-09 12:08:45 -0300

    commenting new forms.

--- BlogApp/forms.py
+++ BlogApp/forms.py
@@ -14,7 +14,16 @@ class EmailPostForm(forms.Form):
                                widget=forms.Textarea)
 
 
+# Instead of using forms.Form to build forms,
+# as we already have models for comments, we can reuse them
+# and create this forms thought those comments models by using
+# forms.ModelForm.
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
+        # All we've to do is specify what models we want to reuse,
+        # listing them in this fields tuple.
+        # Then Django'll create a form with those exactly fields,
+        # and each one of them will inherit its validation properties,
+        # like email, size, etc. And that's freaking cool =D
         fields = ('name', 'email', 'body')
