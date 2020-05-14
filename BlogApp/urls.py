aff8fcb4ceeec6a4af6c5f04338d86fb447ad3c9
NicacioSilva <nas@cesar.school>
2020-05-14 11:09:06 -0300

    organising urls

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -6,8 +6,6 @@ from . import views
 app_name = 'BlogApp'
 
 urlpatterns = [
-    # post views
-    # path('', views.PostListView.as_view(), name='post_list'),
     path('', views.post_list, name='post_list'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
